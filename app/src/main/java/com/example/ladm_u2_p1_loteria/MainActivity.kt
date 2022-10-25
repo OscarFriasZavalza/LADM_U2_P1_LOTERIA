5f40a6cf030e3cfe7075fdc4f75c0d5a079ade6e
friaszavalzao3 <102121487+friaszavalzao3@users.noreply.github.com>
2022-10-24 21:07:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ladm_u2_p1_loteria/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ladm_u2_p1_loteria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
